7453ba5d224feda8f02eb8e3162309e789ef9f43
prateekanandiitb <148332743+prateekanandiitb@users.noreply.github.com>
2025-10-22 18:46:32 +0000

    Deploying to gh-pages from @ prateekanandiitb/prateekanandiitb.github.io@3c20cff90767215e5e564fc0fdbd3c1bf97fde3b 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -18,7 +18,7 @@ ninja.data = [{
           },
         },{id: "nav-projects",
           title: "projects",
-          description: "A growing collection of your cool projects.",
+          description: "Projects underway",
           section: "Navigation",
           handler: () => {
             window.location.href = "/projects/";
