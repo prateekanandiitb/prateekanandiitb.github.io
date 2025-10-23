ee396a0d9cef19946a69a4cd004bac9d3d52c4e4
prateekanandiitb <148332743+prateekanandiitb@users.noreply.github.com>
2025-10-23 14:39:01 +0000

    Deploying to gh-pages from @ prateekanandiitb/prateekanandiitb.github.io@ff7583a7a4de3a6e498180e7448ea1ca9065fec4 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -60,8 +60,8 @@ ninja.data = [{
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
           description: "",
-          section: "News",},{id: "projects-project-1",
-          title: 'project 1',
+          section: "News",},{id: "projects-particle-migration-in-channel-flows",
+          title: 'Particle migration in channel flows',
           description: "with background image",
           section: "Projects",handler: () => {
               window.location.href = "/projects/1_project.html";
