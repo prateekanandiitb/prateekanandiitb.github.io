2ed65b8e5daa9ba27dbc086a0115ac742bde9d0c
prateekanandiitb <148332743+prateekanandiitb@users.noreply.github.com>
2025-10-25 18:41:44 +0000

    Deploying to gh-pages from @ prateekanandiitb/prateekanandiitb.github.io@08e823cdb3ed6234447d8ef6bbb34a527266cac0 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -67,11 +67,6 @@ ninja.data = [{
           description: "",
           section: "Projects",handler: () => {
               window.location.href = "/projects/2_project.html";
-            },},{id: "projects-project-3-with-very-long-name",
-          title: 'project 3 with very long name',
-          description: "a project that redirects to another website",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/3_project.html";
             },},{
         id: 'social-email',
         title: 'email',
