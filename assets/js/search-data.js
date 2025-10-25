853ea906f1093af44a0072137d093321bc831e1a
prateekanandiitb <148332743+prateekanandiitb@users.noreply.github.com>
2025-10-25 18:24:52 +0000

    Deploying to gh-pages from @ prateekanandiitb/prateekanandiitb.github.io@7efbd4f44c9601d148c4b3489a1a945f54a72f47 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -62,9 +62,9 @@ ninja.data = [{
           description: "",
           section: "Projects",handler: () => {
               window.location.href = "/projects/1_project.html";
-            },},{id: "projects-project-2",
-          title: 'project 2',
-          description: "a project with a background image and giscus comments",
+            },},{id: "projects-particle-laden-turbulent",
+          title: 'Particle-laden turbulent',
+          description: "",
           section: "Projects",handler: () => {
               window.location.href = "/projects/2_project.html";
             },},{id: "projects-project-3-with-very-long-name",
