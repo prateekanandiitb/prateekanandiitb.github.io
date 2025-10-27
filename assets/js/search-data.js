bf4f8cbd8cbb62335bb1a1989c7a2f6963c859b2
prateekanandiitb <148332743+prateekanandiitb@users.noreply.github.com>
2025-10-27 12:52:11 +0000

    Deploying to gh-pages from @ prateekanandiitb/prateekanandiitb.github.io@a9f4b0299dab11a16a069a62bf041fb354343a0d 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -4,13 +4,13 @@ const ninja = document.querySelector('ninja-keys');
 // add the home and posts menu items
 ninja.data = [{
     id: "nav-about",
-    title: "about",
+    title: "About",
     section: "Navigation",
     handler: () => {
       window.location.href = "/";
     },
-  },{id: "nav-",
-          title: "",
+  },{id: "nav-group",
+          title: "Group",
           description: "",
           section: "Navigation",
           handler: () => {
@@ -38,7 +38,7 @@ ninja.data = [{
             window.location.href = "/teaching/";
           },
         },{id: "nav-contact",
-          title: "contact",
+          title: "Contact",
           description: "",
           section: "Navigation",
           handler: () => {
